665b6356e8ea67b6455274708a218b19f6d563d6
prajakta <prajakta.kulkarni@tasind.com>
2023-11-08 20:05:22 +0530

    password policy added

--- src/com/tas/wp500/utils/TCPClient.java
+++ src/com/tas/wp500/utils/TCPClient.java
@@ -24,12 +24,7 @@ public class TCPClient {
 			logger.info("Connecting.......");
 			System.out.println("Connecting..");
 			s = new Socket("192.168.1.124", 6801);
-<<<<<<< Updated upstream
-//			s = new Socket("192.168.1.72", 6801);
-=======
-		//	s = new Socket("192.168.1.72", 6801);
->>>>>>> Stashed changes
-		//	s = new Socket("127.0.0.1", 6801);
+
 
 			
 			// new
